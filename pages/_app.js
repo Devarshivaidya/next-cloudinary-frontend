329200f406c450ad8c319342d3502ff75dd65129
devarshivaidya2307@gmail.com <YOUR EMAIL>
2021-07-19 13:35:40 +0530

    final commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,9 @@ import '../styles/globals.css'
 import React from "react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import withData from "../utils/apollo";
-import ErrorBoundary from "../components/ErrorBoundary"
 function MyApp({ Component, pageProps, apollo }) {
   return (
     <ApolloProvider client={apollo}>
-        <ErrorBoundary>
-    <MyWidget />
-  </ErrorBoundary>
       <Component {...pageProps} />
     </ApolloProvider>
     
